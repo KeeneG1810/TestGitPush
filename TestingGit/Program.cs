51ec923d478de718aeebb07d4a743d12db1e0e41
KeeneG1810 <keenegovender1810@gmail.com>
2024-09-09 11:20:54 +0200

    test

---
+++ TestingGit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestingGit
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Testing");
+            Console.WriteLine("Back up data");
+            Console.ReadLine();
+        }
+    }
+}
